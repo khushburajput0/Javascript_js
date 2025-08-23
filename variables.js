2e2f6676f8500f53225cf2b49a776acf29dc19aa
Khushbu Rajput <khushburajput20092005@gmail.com>
2025-08-23 10:47:57 +0530

    variables

---
+++ variables.js
@@ -0,0 +1,12 @@
+const studentId=23012840
+let studentName="Khushbu"
+var studentEmail="khushu@gmail.com"       //not generally preferred to use.
+
+// studentId=123;
+// console.log(studentId)    //const value can't be changed.
+
+studentName="Rishu"
+console.log(studentName)     //value can be changed.
+
+//other way of printing
+console.table([studentId,studentName,studentEmail])
