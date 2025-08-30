bc754a6f0d7344cbf401397a046d045031c25820
Khushbu Rajput <khushburajput20092005@gmail.com>
2025-08-30 18:13:37 +0530

    array

---
+++ 09_arrays.js
@@ -0,0 +1,37 @@
+//Arrays (object)
+/*resizeable
+shallow copies bnata h array-share same reference
+deep copy do not share the same reference */
+
+const myArr=[0,1,2,3,4,5]
+const myHeros=["shaktiman","naagraj"]
+
+const myArr2=new Array(1,2,3,4);
+console.log(myArr[0]);   //assising
+
+//Array methods
+// myArr.push(6)
+// myArr.push(7)
+// console.log(myArr)
+// myArr.pop()        //remove last value
+// myArr.unshift(0)       //add element in starting
+// myArr.shift(3)            //remove 0
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// const newArr=myArr.join();
+
+// console.log(myArr)
+// console.log(newArr)              //convert array into string
+// console.log(typeof(newArr))
+
+//slice,splice
+// console.log("A",myArr);
+// const myn1=myArr.slice(1,3)    //1 index s 2 tk print krega
+// console.log(myn1);
+// console.log("B",myArr)
+// const myn2=myArr.splice(1,3)    //1 s 3 tk print krega
+// console.log(myn2)
+
+
+
