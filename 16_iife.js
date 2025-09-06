d908414224be85d692aa5335c6a3688c6f1c91cf
Khushbu Rajput <khushburajput20092005@gmail.com>
2025-09-06 19:32:36 +0530

    IIFE

--- 16_iife.js
+++ 16_iife.js
@@ -11,3 +11,13 @@
     //unnamed IIFE
     console.log(`DB Connected two ${name}`);
 })('khushu')
+
+
+/*NOTE-JAVASCRIPT EXECUTION CONTEXT
+
+1.GLOBAL EXECUTION CONTEXT -this k andar locate kiya jata h and result result back itno global EC
+2.FUNCTION EXECUTION CONTEXT
+3.EVAL EXECUTION CONTEXT
+
+JAVASCRIPT RUNS IN TWO PHASE I)MEMORY CREATION PHASE-sb variable k nam lekr usme undefined rkha jata h,function k andar definition hati h
+                             II)EXECUTION PHASE*/
